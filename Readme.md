13233c0b5ae1074fd4069050d7a5110683745071
Siddhantg2002 <siddhantg2002@gmail.com>
2024-09-04 00:08:22 +0530

    Deployed

--- Readme.md
+++ Readme.md
@@ -4,3 +4,7 @@
 3. React-syntax-highlighter
 4. React-router-dom
 5. Acerternity UI
+<video width="640" height="360" controls>
+  <source src="../../../SID/Videos//devxp.mp4" type="video/mp4">
+
+</video>
--- src/Components/Error/index.tsx
+++ src/Components/Error/index.tsx
@@ -1,5 +1,5 @@
-
 import { useNavigate } from "react-router-dom";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 export function Error() {
   const navigate = useNavigate();
@@ -10,17 +10,36 @@ export function Error() {
           404
         </h1>
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-md text-center relative z-10">
-        Oops! The page you're looking for doesn't exist.
+          Oops! The page you're looking for doesn't exist.
         </p>
-        <div className="flex justify-center">
-          <button 
-          onClick={()=>{navigate("/")}}
-          className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+        <div className="flex justify-center gap-4">
+          <button
+            onClick={() => {
+              navigate(-1);
+            }}
+            className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block"
+          >
+            <span className="absolute inset-0 overflow-hidden rounded-full">
+              <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+            </span>
+            <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                <IoIosArrowBack className="relative top-[0.5px]"/>
+                <span>Go Back</span>
+            </div>
+            <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+          </button>
+          <button
+            onClick={() => {
+              navigate("/");
+            }}
+            className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block"
+          >
             <span className="absolute inset-0 overflow-hidden rounded-full">
               <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
             </span>
             <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
               <span>Home Page</span>
+              <IoIosArrowForward className="relative top-[0.5px]"/>
             </div>
             <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
           </button>
