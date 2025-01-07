4c7461a2ce044b73cb161f46580946014040cc7e
Siddhantg2002 <siddhantg2002@gmail.com>
2025-01-07 13:47:15 +0530

    Deployment ready

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,7 @@ const server = process.env.SERVER;
 app.use(express.json({ limit: "50mb" })); 
 app.use(
   cors({
-    origin:  process.env.CLIENT, 
+    origin: process.env.CLIENT, 
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
